392a588071a257941e1972f5425581ec2e63a52e
Chris D'Angelo <cd2665@columbia.edu>
2014-02-17 20:17:32 -0500

    missed adding files first commit

---
+++ FlickrContacts/src/main/java/com/chrisdangelo/flickrcontacts/SearchFlickr.java
@@ -0,0 +1,41 @@
+package com.chrisdangelo.flickrcontacts;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class SearchFlickr extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_flickr);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_search_flickr, container, false);
+            return rootView;
+        }
+    }
+
+}
